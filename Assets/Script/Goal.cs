550117905cf95de3d6373dccba2997e6b4155925
Lykan55 <lykan55kai@gmail.com>
2024-09-01 23:44:29 +0900

    あ

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("GameClear");
+    }
+}
