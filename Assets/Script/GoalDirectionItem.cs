8d7e16a1270e4c1481c3387984e18dd6b941f317
Lykan55 <lykan55kai@gmail.com>
2024-09-06 21:42:18 +0900

    itemcheck

--- Assets/Script/GoalDirectionItem.cs
+++ Assets/Script/GoalDirectionItem.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class GoalDirectionItem : MonoBehaviour
 {
+    public ItemManager Items;
     public Transform player;
     public Transform goal;
     public RectTransform arrowUI; // 矢印UI (Imageとして設定)
@@ -32,6 +33,7 @@ public class GoalDirectionItem : MonoBehaviour
 
             // 数秒後にUIを非表示にするコルーチンを開始
             StartCoroutine(HideArrowAfterTime(3f)); // 3秒後に消える
+            Items.ItemCheck = true;
         }
     }
 
