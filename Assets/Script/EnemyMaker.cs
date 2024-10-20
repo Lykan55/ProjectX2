32b90b7518a359fb748f30e011fdda8f2c9f664a
shigeee3 <rokumonsen26@gmail.com>
2024-10-21 00:25:43 +0900

    Many

---
+++ Assets/Script/EnemyMaker.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMaker : MonoBehaviour
+{
+    public GameObject Enemy;
+    public int SummonNumber;
+
+    private List<GameObject> Enemys = new List<GameObject>();
+
+    public void Summon(List<int[]> SummonPos)
+    {
+        if (SummonNumber > SummonPos.Count)
+        {
+            SummonNumber = SummonPos.Count;
+        }
+
+        for (int i = 0; i < SummonNumber; i++)
+        {
+            Vector2 Pos = new Vector2(SummonPos[i][0] * 20, SummonPos[i][1] * 20);
+            Debug.Log(Pos);
+            Enemys.Add(Instantiate(Enemy, Pos, Quaternion.identity));
+            Enemys[i].transform.parent = transform;
+        }
+    }
+
+    public List<GameObject> ReturnEnemys()
+    {
+        return Enemys;
+    }
+}
