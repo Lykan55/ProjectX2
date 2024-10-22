cd44973bdef58048a01294d2ff01bacbe3960d02
shigeee3 <rokumonsen26@gmail.com>
2024-10-22 21:50:19 +0900

    wall

--- Assets/Script/WallDestroyTrigger.cs
+++ Assets/Script/WallDestroyTrigger.cs
@@ -7,14 +7,19 @@ public class WallDestroyTrigger : MonoBehaviour
 {
     private void Start()
     {
-        Destroy(gameObject,0.5f);
+        Destroy(gameObject, 0.5f);
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("wall"))
         {
-            GameObject.Find("enemy 0(Clone)").GetComponent<EnemyMove>().MapNewLoad(collision.transform.position);
+            List<GameObject> Enemys = new List<GameObject>(GameObject.Find("EnemyMaker").GetComponent<EnemyMaker>().ReturnEnemys());
+            for (int i = 0; i < Enemys.Count; i++)
+            {
+                Enemys[i].GetComponent<EnemyMove>().MapNewLoad(collision.transform.position);
+            }
+
             collision.transform.parent.gameObject.SetActive(false);
             Debug.Log("ƒNƒ[ƒ“‚³‚ê‚Ä•Ç‚ğíœ‚µ‚½‚æ");
         }
